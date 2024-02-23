8cb52c538815bd5b31067e714113679a238db843
backendmoon <acms562798@gmail.com>
2024-02-23 13:56:35 +0900

    0223 1356

--- src/main/java/com/zzzpro/zzz/wan/ExController.java
+++ src/main/java/com/zzzpro/zzz/wan/ExController.java
@@ -1,6 +1,5 @@
 package com.zzzpro.zzz.wan;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,9 +7,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import com.zzzpro.zzz.wan.ExController;
 
 import lombok.extern.slf4j.Slf4j;
 
--- src/main/java/com/zzzpro/zzz/wan/ExService.java
+++ src/main/java/com/zzzpro/zzz/wan/ExService.java
@@ -18,7 +18,7 @@ public class ExService {
 		return eDao.linePick(eDto);
 	}
 
-	public List<ExDto> test(ExDto eDto) {
+	public List<ExDto> test(ExDto eDto) { // champions 메서드에서 받아온 챔피언값 필요함 html에서 라인탭누르면 이쪽으로 넘어온 js실행원리
 		eDto.setLine('%' + eDto.getLine() + '%');
 		if (eDto.getLine().equals("%all%")) {
 			return eDao.champions(eDto);
